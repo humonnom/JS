3b749054b6f22941dca15c8822de65e8603ac6ff
humonnom <josojoso0702@gmail.com>
2022-01-06 16:23:58 +0900

    feat: add function/call_by_sharing

---
+++ function/call_by_sharing/test_change.js
@@ -0,0 +1,15 @@
+function changeStuff(a, b, c) {
+  a = a * 10;
+  b.item = "changed";
+  c = { item: "changed" };
+}
+
+var num = 10;
+var obj1 = { item: "unchanged" };
+var obj2 = { item: "unchanged" };
+
+changeStuff(num, obj1, obj2);
+
+console.log(num); //10
+console.log(obj1.item); //changed
+console.log(obj2.item); //unchanged
